5e66be0381a5ac2e8625a235c0ccff98d7d6eb05
pawpaw-cdu <2083658933@qq.com>
2025-10-21 19:41:34 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,17 @@
-//
-//
-//
+//202510302124
+//2083658933@qq.com
+//谢金恩
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main(){
+    float C=0;
+    printf("请输入一个摄氏温度:");
+    scanf("%f",&C);
+
+    float H=C*1.0*9/5+32;
+
+    printf("您输入的摄氏温度为:%.1f\n",C);
+    printf("对应的华氏温度为:%.1f\n",H);
+
     return 0;
 }
